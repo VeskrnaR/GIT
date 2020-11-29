badacb54a7b13d6f8d85eff050dae614490cb0ef
tomasvochyan <tomasvochyan@outlook.cz>
2020-11-29 22:47:36 +0100

    2 - Hotovo

--- P02/Form1.Designer.cs
+++ P02/Form1.Designer.cs
@@ -66,7 +66,7 @@ namespace P02
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(298, 325);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
